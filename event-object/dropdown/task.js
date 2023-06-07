3c529be9bfc5694526a5b9f3c9eb68f84f8d2e5f
WildG <wildgoncher@gmail.com>
2023-06-07 21:47:59 +0300

    crappy solution

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,18 @@
+card = document.querySelector(".card");
+if (card) {
+  list = document.querySelectorAll(".dropdown__item");
+  ul = document.querySelector(".dropdown__list");
+  value = document.querySelector(".dropdown__value");
+  alink = document.querySelectorAll(".dropdown__link");
+
+  value.addEventListener("click", function (e) {
+    ul.classList.toggle("dropdown__list_active");
+  });
+  for (let link of alink) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      ul.classList.toggle("dropdown__list_active");
+      value.textContent = link.textContent;
+    });
+  }
+}
