10c65fa0e4ab59b77327da07db85ec5003239929
WildG <wildgoncher@gmail.com>
2023-06-14 14:53:07 +0300

    new solution

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,24 +1,33 @@
-let elems = document.querySelectorAll(".slider__item");
-let prev_link = document.querySelector(".slider__arrow_prev");
-let next_link = document.querySelector(".slider__arrow_next");
+const prevBtn = document.querySelector('.slider__arrow_prev')
+const nextBtn = document.querySelector('.slider__arrow_next')
+const slides = document.querySelectorAll('.slider__item')
+let activeSlide = 0;
 
-let elem = Array.from(elems);
-let i = 0;
-prev_link.onclick = function () {
-  elem[i].className = "slider__item";
-  i = i - 1;
-  if (i < 0) {
-    i = elems.length - 1;
+function changeSlide() {
+  slides.forEach((el, idx) => {
+      if(idx === activeSlide) {
+          el.classList.add('slider__item_active');
+      } else el.classList.remove('slider__item_active');
+  });
+}
+
+function increaseSlide() {
+  activeSlide += 1
+  if(activeSlide > slides.length - 1) {
+      activeSlide = 0
   }
-  elem[i].className = "slider__item slider__item_active";
-  elem[i - 1].className = "slider__item";
-};
-next_link.onclick = function () {
-  elem[i].className = "slider__item";
-  i = i + 1;
-  if (i >= elems.length) {
-    i = 0;
+  changeSlide()
+}
+
+function decreaseSlide() {
+  activeSlide -= 1
+  if(activeSlide < 0) {
+      activeSlide = slides.length - 1;
   }
-  elem[i].className = "slider__item slider__item_active";
-  elem[i + 1].className = "slider__item";
-};
+  changeSlide()
+}
+
+prevBtn.addEventListener("click", decreaseSlide);
+nextBtn.addEventListener("click", increaseSlide);
+
+changeSlide()
