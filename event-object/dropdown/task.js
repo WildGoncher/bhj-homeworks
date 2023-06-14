16dba09adeccf3112c8e3d15c3753c7d3aa2a537
WildG <wildgoncher@gmail.com>
2023-06-14 15:54:55 +0300

    solution

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,28 +1,25 @@
-const dropdownValue = document.querySelector('.dropdown__value');
-const dropdownList = document.querySelector('.dropdown__list');
-const dropdownLink = document.querySelectorAll('.dropdown__link');
-const dropdown = document.querySelectorAll('.dropdown');
+const dropdownValue = document.querySelector(".dropdown__value");
+const dropdownList = document.querySelector(".dropdown__list");
+const dropdownLink = document.querySelectorAll(".dropdown__link");
+const dropdown = document.querySelectorAll(".dropdown");
 
-
-
-function dropdownClose () { 
-    dropdownValue.addEventListener("click", () =>{
-    dropdownList.classList.toggle('dropdown__list_active');
-    })
+function dropdownClose() {
+  dropdownValue.addEventListener("click", () => {
+    dropdownList.classList.toggle("dropdown__list_active");
+  });
 }
 dropdownClose();
 
-function dropdownUp () {
-    dropdownList.classList.toggle('dropdown__list_active');
+function dropdownUp() {
+  dropdownList.classList.toggle("dropdown__list_active");
 }
 
 let linkArray = Array.from(dropdownLink);
 
 linkArray.forEach((el, i) => {
-
-el.addEventListener('click', (e) =>{
+  el.addEventListener("click", (e) => {
     e.preventDefault();
     dropdownValue.textContent = el.textContent;
-    dropdownUp ();
-});
+    dropdownUp();
+  });
 });
