b4f97bab23e1d0db57aca2def7eb48019e7f8fd7
WildG <wildgoncher@gmail.com>
2023-06-14 14:25:05 +0300

    new solution

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,28 +1,13 @@
-let elems = document.querySelectorAll('.menu .menu_sub')
-let alink = document.querySelectorAll('.menu__link')
-//
-for (let link of alink) {
-  let j = 0
-  let childs = link.childNodes
-  let elem = Array.from(elems)
-  link.onclick = function () {
-    for (let child of childs) {
-      for (let i = 0; i < elem.length; i++) {
-        if (
-          child.textContent ===
-          elem[i].previousSibling.previousSibling.innerText
-        ) {
-          if (elem[i].className == 'menu menu_sub') {
-            elem[i].className += ' menu_active'
-            for (j = 0; j < elem.length; j++) {
-              if (i != j) elem[j].className = 'menu menu_sub'
-            }
-          } else {
-            elem[i].className = 'menu menu_sub'
-          }
-          return false
-        }
-      }
-    }
-  }
-}
+const menuSubs = document.querySelectorAll('.menu_sub');
+const menuItem = document.querySelectorAll('.menu__item');
+const menuLinks = document.querySelectorAll('.menu__link');
+
+menuItem.forEach(elem => {
+    elem.addEventListener('click', (el) => {
+        if (el.target.nextSibling.nextSibling.classList.value === 'menu menu_sub') {
+            el.preventDefault()
+            menuSubs.forEach(elem => elem.classList.remove('menu_active'));
+            el.target.nextSibling.nextSibling.classList.add('menu_active')
+        } 
+    })
+})
