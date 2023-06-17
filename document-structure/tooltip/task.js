d8c8d530c6d3e652deb109b3fac84f821da4a2aa
WildG <wildgoncher@gmail.com>
2023-06-17 11:00:59 +0300

    solution

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -6,14 +6,11 @@ div.className = "tooltip";
 tooltips.forEach((element) => {
   element.addEventListener("click", (event) => {
     event.preventDefault();
-
     target = event.target;
-
     if (target.title === div.innerText) {
       div.classList.toggle("tooltip_active");
       return;
     }
-
     div.innerText = target.title;
     const { bottom, left } = target.getBoundingClientRect();
     div.style = `left: ${left}px; top: ${bottom}px`;
