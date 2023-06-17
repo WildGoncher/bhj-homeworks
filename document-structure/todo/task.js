78ea23042b6166bd0032b572457a56d61dba5dc6
WildG <wildgoncher@gmail.com>
2023-06-17 11:03:23 +0300

    solution

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -7,11 +7,8 @@ function taskCreator() {
     "afterEnd",
     `<div class="task"><div class="task__title">${input.value}</div><a href="#" class="task__remove">&times;</a></div>`
   );
-
   input.value = "";
-
   const closeButtons = document.querySelector(".task__remove");
-
   closeButtons.addEventListener("click", (event) => {
     event.target.closest(".task").remove();
   });
