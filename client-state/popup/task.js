947f684a405124a145987e35b45e287e0ec9e3fa
WildG <wildgoncher@gmail.com>
2023-06-22 13:37:57 +0300

    solution

---
+++ client-state/popup/task.js
@@ -0,0 +1,14 @@
+const modal = document.getElementById('subscribe-modal');
+const closeButton = document.querySelector('.modal__close');
+
+const isModalClosed = document.cookie.includes('modalClosed=true');
+
+if (!isModalClosed) {
+	modal.classList.add('modal_active');
+}
+
+closeButton.addEventListener('click', () => {
+	modal.classList.remove('modal_active');
+
+	document.cookie = 'modalClosed=true'
+});
